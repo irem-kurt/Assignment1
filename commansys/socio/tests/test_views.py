c86692853f01ced834476c7dd79dc5bdd847bedd
Irem Kurt <89161696+irem-kurt@users.noreply.github.com>
2024-05-18 12:42:45 +0300

    Add post creation unit test Closes #29

--- commansys/socio/tests/test_views.py
+++ commansys/socio/tests/test_views.py
@@ -40,14 +40,65 @@ class CommunityCreateViewTest(TestCase):
             'description': 'Test Description',
             'rules': 'Test rules for the community',
             'location': '41.05, 44,10',
-            'is_private': False,  # Assuming it's not private for this test
+            'is_private': False,
             'picture': SimpleUploadedFile(
                 'mock_image.png',
                 image_io.read()
             )  # You can provide a mock image file if needed
         }
         response = self.client.post(reverse('community-create'), data=form_data)
-        self.assertEqual(response.status_code, 200)  # Assuming you're rendering a page upon successful form submission
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(Community.objects.filter(name='Test Community').exists())
 
 
+class CreatePostViewTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        self.test_user1.save()
+
+
+        # Assuming Profile model exists
+        user_profile = Profile.objects.create(user=self.test_user1, name="Test User")
+        user_profile.save()
+        self.test_user1.profile = user_profile
+        self.test_user1.save()
+        # Create a community
+        self.community = Community.objects.create(name='Test Community', description='Test Description', owner=self.test_user1)
+        
+    def test_get_request(self):
+        # Test GET request to the view
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('create_post', args=[self.community.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'socio/create_post.html')
+        self.assertIsInstance(response.context['form'], DefaultPostForm)
+
+    def test_post_request_valid_form(self):
+        # Test POST request with valid form data
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        
+        form_data = {
+            'title': 'Test Title',
+            'description': 'Test Description',
+        }
+        response = self.client.post(reverse('create_post', args=[self.community.id]), data=form_data)
+        
+        # Check if the post was created successfully
+        self.assertEqual(response.status_code, 302) 
+        self.assertTrue(Post.objects.filter(communit_id=self.community, author=self.test_user1).exists())
+    
+
+    def test_post_request_invalid_form(self):
+        # Test POST request with invalid form data
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        
+        form_data = {
+            'title': 'Test Title',  # This is a required field
+        }
+        response = self.client.post(reverse('create_post', kwargs={'community_id': self.community.id}), data=form_data)
+        
+        # Check if the post was not created successfully
+        self.assertNotEqual(response.status_code, 200)
+    
+    
